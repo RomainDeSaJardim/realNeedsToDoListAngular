f1b2a8b10882bd0d8bb8290878e24086b1d4b14f
RomainDeSaJardim <romain.desajardim@gmail.com>
2016-04-09 20:32:51 +0200

    Can create many tasks and delete them

--- controllers/indexCtrl.js
+++ controllers/indexCtrl.js
@@ -1,5 +1,29 @@
 app.controller('indexCtrl', ['$scope', function($scope){
 
-  
+  $scope.initialTask = {
+    name : ""
+  };
+
+  $scope.tasks = [];
+
+  $scope.init = function(){
+    $scope.tasks.push(angular.copy($scope.initialTask));
+  }
+
+  $scope.init();
+
+  $scope.addNewLine = function(newName, originalName){
+    if(originalName === ""){
+      $scope.tasks.push(angular.copy($scope.initialTask));
+      console.log($scope.tasks);
+    }
+    else if(newName === ""){
+      $scope.tasks.splice($scope.tasks.length - 1, 1);
+    }
+  }
+
+  $scope.deleteTask = function(index){
+    $scope.tasks.splice(index, 1);
+  }
 
 }]);
